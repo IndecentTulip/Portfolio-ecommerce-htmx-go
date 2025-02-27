bf3705c692b6223ce06ccd12e9603d144d40f881
IndecentTulip <jowhite2727@gmail.com>
2025-02-26 18:00:23 -0800

    forgot to add this changes to previous commit

--- run.sh
+++ run.sh
@@ -3,5 +3,14 @@
 rm ./database/products.db
 touch ./database/products.db
 
+echo "info related to google auth"
+touch ./src/authcred.txt
+echo "provide Client ID"
+read clientid
+echo "$clientid" > ./src/authcred.txt
+echo "provide Client secret"
+read clientsecret
+echo "$clientsecret" >> ./src/authcred.txt
+
 cd ./src/
-air
+go run main.go
